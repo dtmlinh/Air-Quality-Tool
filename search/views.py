29625d4b67730598b7344444b950cf39311776dc
dtmlinh <linh.t.m.dinh@gmail.com>
2020-11-05 08:48:44 -0600

    commit

--- search/views.py
+++ search/views.py
@@ -53,7 +53,7 @@ class ChartForm(forms.Form):
         required=False, initial=True)
 
     month = forms.ChoiceField(label='Select month for comparison', 
-        choices=[(5, 'May'), (6, 'Jun'), (7, 'Jul'), (8, 'Aug')], required=False)
+        choices=[(6, 'Jun'), (7, 'Jul'), (8, 'Aug')], required=False)
 
     neigh = forms.ChoiceField(label='Show neighborhood maps', 
         choices=NEIGHS, required=False, initial='Hyde Park')
