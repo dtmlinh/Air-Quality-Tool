cad9d9f0c0a250de5ba9c1c87efa01aacf43d824
dtmlinh <linh.t.m.dinh@gmail.com>
2021-01-02 22:53:39 -0600

    change to monthly graphs 2

--- search/views.py
+++ search/views.py
@@ -186,7 +186,7 @@ def home(request):
                     y = args.get('year', None)
 
                     context['month_plot'] = []
-                    for m in (4,5,6,7,8,9)
+                    for m in (4,5,6,7,8,9):
                         f = requests.get(''.join((URL_STATIC, 'graphs/aq/comparison_daily_maps_month_', str(m), '_year_', str(y), '.html')))
                         context['month_plot'].append(f.text)
                 
